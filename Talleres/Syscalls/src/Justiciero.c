68d5b55c194d57dfca6993acc4710adff4517044
Hildorien <nachofernandez.1995@hotmail.com>
2017-03-31 09:49:18 -0300

    ahora los archivos tienen nombre de los ejercicios

---
+++ Talleres/Syscalls/src/Justiciero.c
@@ -0,0 +1,48 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+	int status;
+	pid_t child;
+
+	if (argc <= 1) {
+		fprintf(stderr, "Uso: %s commando [argumentos ...]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Fork en dos procesos */
+	child = fork();
+	if (child == -1) { perror("ERROR fork"); return 1; }
+	if (child == 0) {
+
+		if(ptrace(PTRACE_KILL,0,NULL,NULL))
+		{
+			printf("JUSTICE RAINS FROM ABOVE!!!\n");
+			exit(1);
+		}
+
+		if(ptrace(PTRACE_TRACEME,0,NULL,NULL))
+		{
+			perror("ERROR child ptrace(PTRACE_TRACEME...)");
+			exit(1);
+		}
+		execvp(argv[1], argv+1);
+		/* Si vuelve de exec() hubo un error */
+		perror("ERROR child exec(...)"); exit(1);
+	} else {
+		/* S'olo se ejecuta en el Padre */
+		while(1) {
+			if (wait(&status) < 0) { perror("waitpid"); break; }
+			if (WIFEXITED(status)) break; /* Proceso terminado */
+			ptrace(PTRACE_SYSCALL,child,NULL,NULL); /*Continua */
+		}
+		ptrace(PTRACE_DETACH,child,NULL,NULL); /*Liberamos al hijo */
+	}
+	return 0;
+}
