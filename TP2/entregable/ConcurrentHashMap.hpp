7c57b6e07a25626a8ff468c0c893ab15a7077810
Peter <megalovania@ubuntu>
2017-05-22 00:04:22 -0300

    Maximum implementada

--- TP2/entregable/ConcurrentHashMap.hpp
+++ TP2/entregable/ConcurrentHashMap.hpp
@@ -11,6 +11,14 @@ using namespace std;
 
 class ConcurrentHashMap {
 private:
+
+      struct maximumParams{
+         ConcurrentHashMap *context;
+         int index;
+         int numberThreads;
+         pair<string, unsigned int> solution; 
+      };
+
        vector<  Lista< pair<string, unsigned int> > > tabla; // Segun internet es mejor tener un vector para no tener que de-alocar el arrelgo con delete[].
        //Lista< pair<string, unsigned int> > tabla[TABLE_SIZE];
        pthread_mutex_t mutexLock[TABLE_SIZE];
@@ -50,11 +58,39 @@ private:
          pthread_mutex_lock(&mutexLock[i]);
        }
 
+       void *getMaximumFromRows(void *params){
+         maximumParams *paramsPuntero = (maximumParams*) params;
+         pair<string, unsigned int> maximo;
+         maximo.second = 0;
+         int j = paramsPuntero->index;
+         for (j; j < TABLE_SIZE; j + paramsPuntero->numberThreads )
+         {  
+            Lista< pair<string, unsigned int> >::Iterador it = tabla[j].CrearIt();
+            while(it.HaySiguiente()){
+                  if(it.Siguiente().second > maximo.second){ 
+                   maximo = it.Siguiente();
+                  }
+            it.Avanzar();
+            }
+         }
+         paramsPuntero->solution = maximo;
+       }
+
+       static void *getMaximumFromRowsHelper(void *params){
+         maximumParams *paramsPuntero = (maximumParams*) params;
+         paramsPuntero->context->getMaximumFromRows(params);
+       }
+
+
 public:
 
       ConcurrentHashMap(){
             tabla.resize(TABLE_SIZE); // El hash va a tener size 26
             for (int i = 0; i < TABLE_SIZE; i++){
+               pthread_mutex_init(&mutexLock[i], NULL);
+               pthread_cond_init(&condVarLock[i], NULL);
+               writer[i] = false;
+               currentReaders[i] = 0;
                Lista< pair<string, unsigned int> > vi;
                tabla.push_back(vi);
             }
@@ -101,10 +137,18 @@ public:
       pair<string, unsigned int> maximum(unsigned int nt){
              
          pthread_t thread[nt]; int tid; int i;
+         maximumParams* threadParams[nt];
          int j = nt;
-         i = 0;
-         while( i < TABLE_SIZE && j > 0){
-               j--;
+         pthread_attr_t attr;
+         pthread_attr_init(&attr);
+         pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+         for (i = 0; i < nt; i++)
+         {
+            threadParams[i] = new maximumParams();
+            threadParams[i]->context = this;
+            threadParams[i]->index = i;
+            threadParams[i]->numberThreads = nt; 
+            pthread_create(&thread[i], &attr, getMaximumFromRowsHelper, (void *)threadParams[i]);
          }
          for (tid = 0; tid < nt; ++tid){
                pthread_join(thread[tid], NULL);
